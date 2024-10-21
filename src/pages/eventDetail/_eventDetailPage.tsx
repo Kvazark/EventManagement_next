bfecaee3cce80c0c82a4eb50e63e59cac8e69215
first <Katya_Klebleeva@mail.ru>
2024-10-21 20:26:43 +0300

    feat: init calendar

--- src/pages/eventDetail/_eventDetailPage.tsx
+++ src/pages/eventDetail/_eventDetailPage.tsx
@@ -1,32 +1,33 @@
 // Надо явно указать, что это клиентский компонент, т.к. next думает, что это серверный компонент
 'use client';
 import { Box } from '@mui/material';
-import { GetServerSideProps } from 'next';
 import { Button } from '@/shared/components';
-import { useRouter } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 
 type ConferencePageProps = {
 	id: string;
 };
-export const getServerSideProps: GetServerSideProps = async (context) => {
-	const { id } = context.query;
-	console.log(id);
-	return {
-		props: {
-			id: id as string,
-		},
-	};
-	//const conference = await getEventByID(id as string);
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+// 	const { id } = context.params!;
+//
+// 	if (!id) {
+// 		return {
+// 			notFound: true,
+// 		};
+// 	}
+//
+// 	return {
+// 		props: {
+// 			id: id as string,
+// 		},
+// 	};
+// };
 
-	// return {
-	// 	props: {
-	// 		conference,
-	// 	},
-	// };
-};
-
-export const EventDetailPage = ({ id }: ConferencePageProps) => {
+export const EventDetailPage = () => {
 	const router = useRouter();
+	const params = useParams();
+
+	const id = params?.id;
 
 	return (
 		<Box>
